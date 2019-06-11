73bbcfeac26658e400ef1ba896c8d76ee4ab5a26
Yura <ybeliaev@gmail.com>
2019-06-11 19:20:13 +0300

    some

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox1</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello !</h1>
+      <h2>Пример простого клика:</h2>
     </div>
   );
 }
