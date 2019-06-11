9e5d9ab90d6fec2585d1b9c2bfa96f196abe1de4
Yura <ybeliaev@gmail.com>
2019-06-11 20:57:21 +0300

    fin

--- src/list-item.js
+++ src/list-item.js
@@ -3,9 +3,12 @@ import React from "react";
 export default class ListItem extends React.Component {
   constructor() {
     super();
+    this.onLabelClick = () => {
+      console.log(`This label: ${this.props.label}`);
+    };
   }
   render() {
     const { label, important = false } = this.props;
-    return <span>{label}</span>;
+    return <span onClick={this.onLabelClick}>{label}</span>;
   }
 }
